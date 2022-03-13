3412ce99185b6b7258549e3a266362747b6767ad
Artur Gawron <arturgawron6@gmail.com>
2022-03-13 11:18:22 +0100

    Vanilla app

---
+++ src/test/java/p/lodz/pl/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package p.lodz.pl.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
