04550f907e999a45a111b15848fad8a64c957351
Artur Gawron <arturgawron6@gmail.com>
2022-03-13 13:48:46 +0100

    add rest crud for TodoPoint

---
+++ src/main/java/p/lodz/pl/todoapp/entities/TodoPoint.java
@@ -0,0 +1,46 @@
+package p.lodz.pl.todoapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class TodoPoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
